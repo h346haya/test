017849865dcde8e59b7ec32732fbc74e8b4ae917
h346haya <108774372+h346haya@users.noreply.github.com>
2022-07-13 11:17:11 +0900

    Add files via upload

---
+++ a.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php 
+$b='abc';
+  foreach(file('/usr/share/dict/linux.words') as $line){
+    $pos=strpos($line,$b);
+    if ($pos !== false){
+     echo $line;
+    }
+    //関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
+  } 
+?>
